b10dd1cc088f54d5d437b9ffa495301d41b2a03c
rmorrill <morrill.ryan@gmail.com>
2022-08-12 18:43:26 -0400

    Delete demo directory

--- demo/isNotInCircle.m
+++
@@ -1,3 +0,0 @@
-function notInCircle = isNotInCircle(xPts, yPts, center, radius) 
-% supporting function for AVtrainerStim system
-notInCircle = (xPts - center(1)).^2 + (yPts - center(2)).^2 > radius^2; 
--- demo/isNotInRect.m
+++
@@ -1,4 +0,0 @@
-function notInRect = isNotInRect(xPts, yPts, center, rectWdth, rectHt) 
-% supporting function for AVtrainerStim system
-notInRect = xPts >= center(1)+rectWdth/2 | xPts <= center(1)-rectWdth/2 | ...
-        yPts >= center(2)+rectHt/2 | yPts <= center(2)-rectHt/2; 
